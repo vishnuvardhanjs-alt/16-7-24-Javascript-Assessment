bf47e2becb4c09161c15e03e9c95d2985b68832f
vishnuvardhanjs-alt <vichudsvv@gmail.com>
2024-07-16 16:39:50 +0530

    Assessment Done

---
+++ manupulation.js
@@ -0,0 +1,21 @@
+let b = ["this", "is", "an", "array"]; //initializing array
+
+console.log("Original List");
+console.log(b);
+
+b.push("end"); //push to add the elements to the end
+
+console.log("Array Push");
+console.log(b);
+
+b.pop(); //pop to remove elements from the end
+console.log("Array pop");
+console.log(b);
+
+b.unshift("Hello"); //array unshifting to add to first
+console.log("Array unShift");
+console.log(b);
+
+b.shift(); //array shifting operation to remove from first
+console.log("Array SHift");
+console.log(b);
---
+++ merge.js
@@ -0,0 +1,13 @@
+let a = ["hello", "world"]; //array initializing
+
+let b = ["this", "is", "an", "array"];
+
+let c = a.concat(b); //concatenatig array b with a
+
+console.log("List 1");
+console.log(a);
+console.log("List 2");
+console.log(b);
+
+console.log("Merged List");
+console.log(c);
